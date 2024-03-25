9cd713600be4dd7d4d0e7684d8849780245d0290
John Kiritsis <yiannikir@protonmail.com>
2024-03-25 08:31:27 +0200

    dfsdaf

--- src/shaderHeader.h
+++ src/shaderHeader.h
@@ -1,7 +1,21 @@
 
 #ifndef SHADERHEADER_H
 #define SHADERHEADER_H
-#include "TSP.h"
+#include "vendors/glad/glad.h"
+#include "vendors/GLFW/glfw3.h"
+#include "vendors/glm/glm.hpp"
+#include "vendors/glm/gtc/matrix_transform.hpp"
+#include "vendors/glm/gtc/type_ptr.hpp"
+
+#include <string>
+
+#include <unistd.h>
+#include <cmath>
+
+#include <algorithm>
+
+#include <sstream>
+#include <iostream>
 
 #include <fstream>
 
