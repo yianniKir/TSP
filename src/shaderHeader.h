6df03683cb2424fccffcb607370bb1faf180e8d1
John Kiritsis <yiannikir@protonmail.com>
2024-03-25 08:34:30 +0200

    changed includes

--- src/shaderHeader.h
+++ src/shaderHeader.h
@@ -1,21 +1,8 @@
 
 #ifndef SHADERHEADER_H
 #define SHADERHEADER_H
-#include "vendors/glad/glad.h"
-#include "vendors/GLFW/glfw3.h"
-#include "vendors/glm/glm.hpp"
-#include "vendors/glm/gtc/matrix_transform.hpp"
-#include "vendors/glm/gtc/type_ptr.hpp"
 
-#include <string>
-
-#include <unistd.h>
-#include <cmath>
-
-#include <algorithm>
-
-#include <sstream>
-#include <iostream>
+#include "TSP.h"
 
 #include <fstream>
 
